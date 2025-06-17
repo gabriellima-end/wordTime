58867bbc67f6efcd2131d6803f94cc8119dcebec
Gabriel Lima <106500760+gabriellima-end@users.noreply.github.com>
2025-06-17 19:36:23 -0300

    Add files via upload

--- wordTime/src/wordTime/Main.java
+++ wordTime/src/wordTime/Main.java
@@ -2,7 +2,9 @@ package wordTime;
 
 import java.time.Duration;
 import java.time.Instant;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -21,7 +23,13 @@ public class Main {
 		
 		Scanner scanner = new Scanner(System.in);
 		
+		List<String> listaPalavraSelecionada = new ArrayList<>();
 		
+		List<String> listaPalavrasDigitadas = new ArrayList<>();
+		
+		int score = 0;
+		
+		//DESENVOLVER MENU DE ESCOLHA DO TEMPO MINIMO
 		
 		while(true) {
 			
@@ -29,16 +37,21 @@ public class Main {
 			
 			System.out.println("A palavra selecionada é: "+randomWord.palavraSelecionada.toUpperCase() + "\n");
 			
+			//ADICIONANDO AS PALAVRAS SELECIONADAS NA LISTA
+			listaPalavraSelecionada.add(randomWord.palavraSelecionada.toUpperCase());
+			
 			System.out.println("Digite a palavra: ");
 			
 			System.out.println();
-			
-			
 			
 			//CONTANDO O TEMPO QUE SE DIGITA A PALAVRA
 			Instant startTime = Instant.now();
 			
 			String typedWord = scanner.nextLine();
+			System.out.println();
+			
+			//ADICIONANDO AS PALAVRAS DIGITADAS NA LISTA
+			listaPalavrasDigitadas.add(typedWord.toUpperCase());
 			
 			//FIM DA CONTAGEM
 			Instant finishTime = Instant.now();
@@ -49,6 +62,7 @@ public class Main {
 			float timeDuration = duration.toMillis();
 			
 			System.out.println("Tempo decorrido: " + duration.toMillis() + "ms");
+			System.out.println();
 			
 			if(timeDuration > 5000) {
 				
@@ -60,5 +74,21 @@ public class Main {
 			}
 		
 		}
+		
+		//VERIFICANDO SE AS PALAVRAS ESTÃO IGUAIS E PONTUANDO
+		for(int i = 0; i<listaPalavraSelecionada.size(); i++) {
+			
+			if(listaPalavraSelecionada.get(i).equals(listaPalavrasDigitadas.get(i))) {
+				
+				score += 50;
+				
+			}
+			
+		}
+		
+		System.out.println(listaPalavraSelecionada);
+		System.out.println(listaPalavrasDigitadas);
+		System.out.println("Sua pontuação: " + score);
+		
 	}
 }
